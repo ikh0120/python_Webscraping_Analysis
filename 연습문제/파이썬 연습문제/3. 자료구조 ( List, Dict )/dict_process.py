b03c37f197eb8354b9782d5006cd117440d251a8
ikh0120 <lkh50635164@gmail.com>
2025-04-13 23:45:57 +0900

    딕셔너리별로 분류 및 dict_process.py 추가

---
+++ 연습문제/파이썬 연습문제/3. 자료구조 ( List, Dict )/dict_process.py
@@ -0,0 +1,9 @@
+dict1 = {"name": "John", "age": 30}
+print("나이:", dict1['age'])
+
+dict2= {"math": 90, "science": 85, "history": 78}
+print("과목들:", list(dict2.keys()))
+
+dict3 = {'apple': 3, 'banana': 5}
+dict3['apple'] += 2
+print(dict3)
---
+++ 연습문제/파이썬 연습문제/3. 자료구조 ( List, Dict )/list_process.py
@@ -0,0 +1,14 @@
+arr1= ["apple", "banana", "cherry"]
+arr1.append("orange")
+print(arr1)
+
+arr2= [10, 20, 30]
+print("합계: ",sum(arr2))
+
+arr3 =  [1, 2, 3, 4, 5]
+arr3.sort(reverse=True)
+print(arr3)
+
+arr4= [5, 2, 8, 1, 9]
+arr4.sort(reverse=False)
+print(arr4)
