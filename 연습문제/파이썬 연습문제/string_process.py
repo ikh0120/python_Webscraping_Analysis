01a01cbdb9bbd59a3ac611e32662f4c496191395
ikh0120 <lkh50635164@gmail.com>
2025-04-13 23:15:19 +0900

    string_process.py

---
+++ 연습문제/파이썬 연습문제/string_process.py
@@ -0,0 +1,23 @@
+first="Hello"
+secound="World"
+print(f"{first} {secound}")
+
+str = first + " " + secound
+print(str.upper())
+
+print(str[6:])
+
+
+str_ = "Python is fun"
+print(str_.split())
+
+str__ = "abcdef"
+string=""
+for i in range(0,6,2):
+    string += str__[i]
+print(string)
+
+result = ""
+for i in range(3):
+    result+=first
+print(result)
